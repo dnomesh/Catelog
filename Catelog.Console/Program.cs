97c7577dacfc3f9cbbe5abb1654eee467d9c84a5
Nomesh Divi <nomesh27@gmail.com>
2025-02-27 01:22:06 +0530

    comments are removed.

--- Catelog.Console/Program.cs
+++ Catelog.Console/Program.cs
@@ -10,19 +10,6 @@ string fileName = Path.Combine("App_Data", "CatelogData.xlsx");
 ICatelog catelog = new CatelogService(db);
 
 Console.WriteLine("Data Insertion Started...");
-//InitializeVariables();
 int result = catelog.ReadMigrationDataFromExcelFile(fileName);
 Console.WriteLine("Data Insertion Ended...");
-
-
-//void InitializeVariables()
-//{
-//    string jsonFileName = "appsettings.json";
-//    var config = new ConfigurationBuilder().AddJsonFile(jsonFileName, true, true);
-//    var configurationRoot = config.Build();
-//    catelogConnectionString = configurationRoot.GetSection("ConnectionStrings").GetSection("Catelog").Value;
-//    db = new CatelogDbContext();
-//}
-
-
 
